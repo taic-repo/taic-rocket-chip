0e1299d858153398ced07bfbbba94066168a218d
zflcs <1491657576@qq.com>
2025-01-05 12:34:17 +0800

    Build rocket-chip with 4 core.

--- taic/src/main/scala/Config.scala
+++ taic/src/main/scala/Config.scala
@@ -40,7 +40,7 @@ class WithNEBigCores(n: Int, overrideIdOffset: Option[Int] = None) extends Confi
 })
 
 class TaicConfig extends Config(
-  new WithNEBigCores(2) ++
+  new WithNEBigCores(4) ++
     new WithNExtTopInterrupts(6) ++
     new WithTimebase((BigInt(10000000))) ++ // 10 MHz
     new WithDTS("freechips.rocketchip-unknown", Nil) ++
